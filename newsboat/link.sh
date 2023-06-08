377c8c18b04ccb45611f56731055bf45176d361e
Data Salaryman <datasalaryman@protonmail.com>
2023-06-08 14:09:26 +0800

    build(newsboat): add script to symlink newsboat urls file to this file

---
+++ newsboat/link.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+if [[ "${TRACE-0}" == "1" ]]; then
+    set -o xtrace
+fi
+
+if [[ "${1-}" =~ ^-*h(elp)?$ ]]; then
+    echo 'Usage: ./link.sh
+
+Symlink the newsboat urls to this github repository
+
+'
+    exit
+fi
+
+main() {
+  if [[ ! -d "$HOME/.newsboat" ]]
+    then
+      mkdir "$HOME/.newsboat"
+      ln -s "$PWD/newsboat/urls" "$HOME/.newsboat/urls"
+  fi
+
+  if [[ ! -f "$HOME/.newsboat/urls" ]]
+    then
+      ln -s "$PWD/newsboat/urls" "$HOME/.newsboat/urls"
+    else
+      rm "$HOME/.newsboat/urls"
+      ln -s "$PWD/newsboat/urls" "$HOME/.newsboat/urls"
+  fi
+}
+
+main "$@"
